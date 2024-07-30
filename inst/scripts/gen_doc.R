bf9300371ae0a857ef507f7f15d6c13c246fb58e
Maxime Charriere <51918753+maximecharriere@users.noreply.github.com>
2024-07-31 01:21:25 +0200

    update doc

--- inst/scripts/gen_doc.R
+++
@@ -1,5 +0,0 @@
-# First install pandoc https://github.com/jgm/pandoc/releases
-devtools::document() # Equivalent to roxygenize(), also updates documents
-devtools::build() # Build the package
-devtools::install() # Install the package to your R library
-pkgdown::build_site()
